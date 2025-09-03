109bd8406f6356293e0151f03743fb3a52619b82
jpmartinezt <jp.martinez2@uniandes.edu.co>
2025-09-03 06:20:10 -0500

    creacion logica, presentacion y doc

---
+++ Parcial1/src/presentacion/Principal.java
@@ -0,0 +1,21 @@
+package presentacion;
+//import logica.;
+
+public class Principal {
+
+	//private abstract_type name;
+	
+	public Principal() {
+		try {
+			//this.name.method;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		new Principal();
+	}
+
+}
